92f674f63038a3bbc35a1ff463a728f6127bd64a
Mauro <maurotapiadev@gamil.com>
2024-11-07 20:23:53 -0300

    continuing to work with registration

--- usersService/src/main/java/com/homeBanking/usersService/security/KeycloakJwtAuthConverter.java
+++ usersService/src/main/java/com/homeBanking/usersService/security/KeycloakJwtAuthConverter.java
@@ -2,10 +2,11 @@ package com.homeBanking.usersService.security;
 
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.oauth2.jwt.Jwt;
 
-import java.util.Collection;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class KeycloakJwtAuthConverter implements Converter<Jwt, Collection<GrantedAuthority>> {
 
@@ -18,6 +19,50 @@ public class KeycloakJwtAuthConverter implements Converter<Jwt, Collection<Grant
 
     @Override
     public Collection<GrantedAuthority> convert(Jwt source) {
-        return List.of();
+        Collection<GrantedAuthority> authorities = new ArrayList<>();
+
+
+        Map<String, Map<String, List<String>>> resourcesAccess =
+                (Map<String, Map<String, List<String>>>) source.getClaims().get(RESOURCE_ACCESS_CLAIM);
+        if (resourcesAccess != null && !resourcesAccess.isEmpty()) {
+            Map<String, List<String>> clientAccessRoles = resourcesAccess.get(USERS_CLAIM);
+            authorities.addAll(clientAccessRoles != null ? extractRoles(clientAccessRoles) : List.of());
+        }
+
+        String scopes = (String) source.getClaims().get(SCOPES_CLAIM);
+        if (scopes != null) {
+            authorities.addAll(extractScopes(scopes));
+        }
+
+        List<String> groups = (List<String>) source.getClaims().get(GROUPS_CLAIM);
+        if (groups != null && !groups.isEmpty()) {
+            authorities.addAll(extractGroups(groups));
+        }
+
+
+        return authorities;
+
+    }
+
+
+    private static Collection<GrantedAuthority> extractRoles(Map<String, List<String>> roles) {
+        return  roles.get(ROLES_CLAIM)
+                .stream()
+                .map(role -> new SimpleGrantedAuthority("ROLE_" + role))
+                .collect(Collectors.toList());
+    }
+
+    private static Collection<GrantedAuthority> extractScopes(String scopes) {
+        return Arrays.stream(scopes.split(" "))
+                .map(scope -> new SimpleGrantedAuthority("SCOPE_" + scope))
+                .collect(Collectors.toList());
+    }
+
+    private static Collection<GrantedAuthority> extractGroups(List<String> groups) {
+        return groups.stream()
+                .map(groupName -> groupName.substring(1))
+                .map(groupName -> "GROUP_" + groupName)
+                .map(SimpleGrantedAuthority::new)
+                .collect(Collectors.toList());
     }
 }
